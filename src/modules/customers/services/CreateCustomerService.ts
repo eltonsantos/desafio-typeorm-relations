4fb8f6a1de1e9b04d862f304938d6d64c58dfeba
Elton Santos <eltonaxl@hotmail.com>
2021-01-28 00:08:56 -0300

    Commenting line of AppError

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
+// eslint-disable-next-line import/no-unresolved
 import AppError from '@shared/errors/AppError';
 
 import Customer from '../infra/typeorm/entities/Customer';
